8c08e43da5600df393e0ec736a823a35192d32d9
caizl <caizl2002@hotmail.com>
2019-01-23 17:28:22 +0800

    rewrite VtOrderReq Class, remove useless para

--- vnpy/trader/vtObject.py
+++ vnpy/trader/vtObject.py
@@ -479,18 +479,17 @@ class VtOrderReq(object):
         self.price = EMPTY_FLOAT                # 价格
         self.volume = EMPTY_INT                 # 数量
     
-        self.priceType = EMPTY_STRING           # 价格类型
+        self.orderType = EMPTY_STRING           # 市价，限价
         self.direction = EMPTY_STRING           # 买卖
-        self.offset = EMPTY_STRING              # 开平
         
         # 以下为IB相关
-        self.productClass = EMPTY_UNICODE       # 合约类型
-        self.currency = EMPTY_STRING            # 合约货币
-        self.expiry = EMPTY_STRING              # 到期日
-        self.strikePrice = EMPTY_FLOAT          # 行权价
-        self.optionType = EMPTY_UNICODE         # 期权类型     
-        self.lastTradeDateOrContractMonth = EMPTY_STRING   # 合约月,IB专用
-        self.multiplier = EMPTY_STRING                     # 乘数,IB专用
+        #self.productClass = EMPTY_UNICODE       # 合约类型
+        #self.currency = EMPTY_STRING            # 合约货币
+        #self.expiry = EMPTY_STRING              # 到期日
+        #self.strikePrice = EMPTY_FLOAT          # 行权价
+        #self.optionType = EMPTY_UNICODE         # 期权类型
+        #self.lastTradeDateOrContractMonth = EMPTY_STRING   # 合约月,IB专用
+        #self.multiplier = EMPTY_STRING                     # 乘数,IB专用
         
 
 ########################################################################
