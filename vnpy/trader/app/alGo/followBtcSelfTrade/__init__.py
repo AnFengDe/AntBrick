cde73772f1c6fedc7de9b6c4aefa5f8fd0c10b25
caizl <caizl2002@hotmail.com>
2019-02-25 17:38:56 +0800

    first commit

---
+++ vnpy/trader/app/alGo/followBtcSelfTrade/__init__.py
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+from __future__ import absolute_import
+from .followBtcEngine import FollowBtcEngine
+from .uiFollowBtcWidget import FollowBtcEngineManager
+
+appName = 'FollowBtcSelfTrade'
+appDisplayName = u'跟随BTC刷单'
+appEngine = FollowBtcEngine
+appWidget = FollowBtcEngineManager
+appIco = 'at.ico'
