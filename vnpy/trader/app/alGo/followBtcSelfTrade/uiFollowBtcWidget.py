8f8cc3a03a7915f70006e083e5a6d731964770c4
caizl <caizl2002@hotmail.com>
2019-02-26 19:00:00 +0800

    add some para

--- vnpy/trader/app/alGo/followBtcSelfTrade/uiFollowBtcWidget.py
+++ vnpy/trader/app/alGo/followBtcSelfTrade/uiFollowBtcWidget.py
@@ -7,7 +7,24 @@
 from vnpy.trader.uiBasicWidget import QtWidgets
 
 
-#from vnpy.trader.app.alGo.followBtcSelfTrade.language import text
+class HorizonSplitLine(QtWidgets.QFrame):
+    # 水平分割线
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.setFrameShape(self.HLine)
+        self.setFrameShadow(self.Sunken)
+
+
+class SplitGrid(QtWidgets.QFormLayout):
+    # 水平分割线
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        lineSplit = HorizonSplitLine()
+        self.addWidget(lineSplit)
+
 
 ########################################################################
 class FollowBtcEngineManager(QtWidgets.QWidget):
@@ -34,40 +51,71 @@ class FollowBtcEngineManager(QtWidgets.QWidget):
         except Exception as e:
             print(e)
 
-        # self.lineSymbol = QtWidgets.QLineEdit()
+        # 报价相关设置
         self.lineSymbolBasicPrice = QtWidgets.QLineEdit()
+        self.lineSymbolMinPrice = QtWidgets.QLineEdit()
+        self.lineSymbolMaxPrice = QtWidgets.QLineEdit()
         self.lineBtcBasicPrice = QtWidgets.QLineEdit()
         self.lineOrderLevel = QtWidgets.QLineEdit()
         self.lineOrderPriceGap = QtWidgets.QLineEdit()
         self.lineOrderVolume = QtWidgets.QLineEdit()
         self.lineBtcCheckInterval = QtWidgets.QLineEdit()
-        #self.linePlaceOrderInterval = QtWidgets.QLineEdit()
+        self.linePriceRatio = QtWidgets.QLineEdit()
+
+        Label = QtWidgets.QLabel
+
+        gridBtcCheck = QtWidgets.QGridLayout()
+        gridBtcCheck.addWidget(Label(u'交易对代码'), 0, 0)
+        gridBtcCheck.addWidget(self.comboVtSymbol, 0, 1)
+        gridBtcCheck.addWidget(Label(u'交易对基准价'), 1, 0)
+        gridBtcCheck.addWidget(self.lineSymbolBasicPrice, 1, 1)
+        gridBtcCheck.addWidget(Label(u'交易对最低价'), 2, 0)
+        gridBtcCheck.addWidget(self.lineSymbolMinPrice, 2, 1)
+        gridBtcCheck.addWidget(Label(u'交易对最高价'), 3, 0)
+        gridBtcCheck.addWidget(self.lineSymbolMaxPrice, 3, 1)
+        gridBtcCheck.addWidget(Label(u'比特币基准价'), 4, 0)
+        gridBtcCheck.addWidget(self.lineBtcBasicPrice, 4, 1)
+        gridBtcCheck.addWidget(Label(u'买卖档数'), 5, 0)
+        gridBtcCheck.addWidget(self.lineOrderLevel, 5, 1)
+        gridBtcCheck.addWidget(Label(u'档间价差'), 6, 0)
+        gridBtcCheck.addWidget(self.lineOrderPriceGap, 6, 1)
+        gridBtcCheck.addWidget(Label(u'每档挂单数量'), 7, 0)
+        gridBtcCheck.addWidget(self.lineOrderVolume, 7, 1)
+        gridBtcCheck.addWidget(Label(u'比特币检查时间间隔'), 8, 0)
+        gridBtcCheck.addWidget(self.lineBtcCheckInterval, 8, 1)
+        gridBtcCheck.addWidget(Label(u'价格波动倍数'), 9, 0)
+        gridBtcCheck.addWidget(self.linePriceRatio, 9, 1)
+
+        gridSplit = SplitGrid()
+
+        # 刷单相关设置
+        gridSelfTrade = QtWidgets.QGridLayout()
         self.lineSelfTradeVolume = QtWidgets.QLineEdit()
+        self.lineSelfTradeInterval = QtWidgets.QLineEdit()
+        self.lineSelfTradeDayVolume = QtWidgets.QLineEdit()
+
+        gridSelfTrade.addWidget(Label(u'每次刷单平均数量'), 0, 0)
+        gridSelfTrade.addWidget(self.lineSelfTradeVolume, 0, 1)
+        gridSelfTrade.addWidget(Label(u'刷单间隔时间'), 1, 0)
+        gridSelfTrade.addWidget(self.lineSelfTradeInterval, 1, 1)
+        gridSelfTrade.addWidget(Label(u'预估每日刷单数量'), 2, 0)
+        gridSelfTrade.addWidget(self.lineSelfTradeDayVolume, 2, 1)
 
         self.buttonSaveSetting = QtWidgets.QPushButton(u'保存配置')
         #buttonActivate = QtWidgets.QPushButton(u'激活算法')
         self.buttonSwitchEngineStatus = QtWidgets.QPushButton(u'BTC跟随报价刷单引擎停止')
-        Label = QtWidgets.QLabel
 
-        grid = QtWidgets.QGridLayout()
-        grid.addWidget(Label(u'交易对代码'), 0, 0)
-        grid.addWidget(self.comboVtSymbol, 0, 1)
-        grid.addWidget(Label(u'交易对基准价'), 1, 0)
-        grid.addWidget(self.lineSymbolBasicPrice, 1, 1)
-        grid.addWidget(Label(u'比特币基准价'), 2, 0)
-        grid.addWidget(self.lineBtcBasicPrice, 2, 1)
-        grid.addWidget(Label(u'买卖档数'), 3, 0)
-        grid.addWidget(self.lineOrderLevel, 3, 1)
-        grid.addWidget(Label(u'档间价差'), 4, 0)
-        grid.addWidget(self.lineOrderPriceGap, 4, 1)
-        grid.addWidget(Label(u'每档挂单数量'), 5, 0)
-        grid.addWidget(self.lineOrderVolume, 5, 1)
-        grid.addWidget(Label(u'比特币检查时间间隔'), 6, 0)
-        grid.addWidget(self.lineBtcCheckInterval, 6, 1)
-        #grid.addWidget(Label(u'重新报价时间间隔范围'), 7, 0)
-        #grid.addWidget(self.linePlaceOrderInterval, 7, 1)
-        grid.addWidget(Label(u'每次刷单数量'), 8, 0)
-        grid.addWidget(self.lineSelfTradeVolume, 8, 1)
+        hbox = QtWidgets.QHBoxLayout()
+        hbox.addStretch()
+        hbox.addWidget(self.buttonSaveSetting)
+        hbox.addWidget(self.buttonSwitchEngineStatus)
+
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addLayout(gridBtcCheck)
+        vbox.addLayout(gridSplit)
+        vbox.addLayout(gridSelfTrade)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
 
         self.getsettingForVTSymbol()  # 根据vtsymbol读取配置
 
@@ -75,19 +123,6 @@ class FollowBtcEngineManager(QtWidgets.QWidget):
         self.comboVtSymbol.currentIndexChanged.connect(self.getsettingForVTSymbol)
         self.buttonSaveSetting.clicked.connect(self.saveSettingForVTSymbol)
         self.buttonSwitchEngineStatus.clicked.connect(self.switchEngineStatus)
-
-        hbox = QtWidgets.QHBoxLayout()
-        #hbox.addWidget(self.buttonClearOrderFlowCount)
-        #hbox.addWidget(self.buttonClearTradeCount)
-        hbox.addStretch()
-        hbox.addWidget(self.buttonSaveSetting)  # 不能用self
-        #hbox.addWidget(buttonActivate)
-        hbox.addWidget(self.buttonSwitchEngineStatus)
-
-        vbox = QtWidgets.QVBoxLayout()
-        vbox.addLayout(grid)
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
 
     def getsettingForVTSymbol(self):
         """根据交易对读取配置信息"""
