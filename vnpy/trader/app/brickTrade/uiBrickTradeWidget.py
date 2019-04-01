564a9a0765b891c28fad20fc54898480e12d5cf2
yangzhi <@4F!xZpJwly&KbWq>
2019-04-01 19:32:16 +0800

    简单搬砖逻辑

--- vnpy/trader/app/brickTrade/uiBrickTradeWidget.py
+++ vnpy/trader/app/brickTrade/uiBrickTradeWidget.py
@@ -67,59 +67,59 @@ class BrickTradeManager(QtWidgets.QWidget):
         Label = QtWidgets.QLabel
 
         gridBtcCheck = QtWidgets.QGridLayout()
-        gridBtcCheck.addWidget(Label(u'交易对代码'), 0, 0)
-        gridBtcCheck.addWidget(self.comboVtSymbol, 0, 1)
-        gridBtcCheck.addWidget(Label(u'交易对基准价'), 1, 0)
-        gridBtcCheck.addWidget(self.lineSymbolBasicPrice, 1, 1)
-        gridBtcCheck.addWidget(Label(u'交易对最低价'), 2, 0)
-        gridBtcCheck.addWidget(self.lineSymbolMinPrice, 2, 1)
-        gridBtcCheck.addWidget(Label(u'交易对最高价'), 3, 0)
-        gridBtcCheck.addWidget(self.lineSymbolMaxPrice, 3, 1)
-        gridBtcCheck.addWidget(Label(u'比特币基准价(USDT)'), 4, 0)
-        gridBtcCheck.addWidget(self.lineBtcBasicPrice, 4, 1)
-        gridBtcCheck.addWidget(Label(u'买卖档数'), 5, 0)
-        gridBtcCheck.addWidget(self.lineOrderLevel, 5, 1)
-        gridBtcCheck.addWidget(Label(u'档间价差倍数'), 6, 0)
-        gridBtcCheck.addWidget(self.lineOrderPriceGap, 6, 1)
-        gridBtcCheck.addWidget(Label(u'每档挂单数量上限'), 7, 0)
-        gridBtcCheck.addWidget(self.lineOrderVolumeMax, 7, 1)
-        gridBtcCheck.addWidget(Label(u'每档挂单数量下限'), 8, 0)
-        gridBtcCheck.addWidget(self.lineOrderVolumeMin, 8, 1)
-        gridBtcCheck.addWidget(Label(u'比特币检查时间间隔(秒)'), 9, 0)
-        gridBtcCheck.addWidget(self.lineBtcCheckInterval, 9, 1)
-        gridBtcCheck.addWidget(Label(u'价格波动倍数'), 10, 0)
-        gridBtcCheck.addWidget(self.linePriceRatio, 10, 1)
-
-        gridSplit = SplitGrid()
-
-        # 刷单相关设置
-        gridSelfTrade = QtWidgets.QGridLayout()
-        self.lineSelfTradeVolume = QtWidgets.QLineEdit()
-        self.lineSelfTradeInterval = QtWidgets.QLineEdit()
-        self.lineSelfTradeDayVolume = QtWidgets.QLineEdit()
-
-        gridSelfTrade.addWidget(Label(u'每次刷单平均数量'), 0, 0)
-        gridSelfTrade.addWidget(self.lineSelfTradeVolume, 0, 1)
-        gridSelfTrade.addWidget(Label(u'刷单间隔时间(秒)'), 1, 0)
-        gridSelfTrade.addWidget(self.lineSelfTradeInterval, 1, 1)
-        gridSelfTrade.addWidget(Label(u'预估每日刷单数量'), 2, 0)
-        gridSelfTrade.addWidget(self.lineSelfTradeDayVolume, 2, 1)
-        self.lineSelfTradeDayVolume.setStyleSheet("color: yellow")
-
-        self.buttonSaveSetting = QtWidgets.QPushButton(u'保存配置')
-        self.buttonSaveSetting.setStyleSheet("background-color: blue")
+        # gridBtcCheck.addWidget(Label(u'交易对代码'), 0, 0)
+        # gridBtcCheck.addWidget(self.comboVtSymbol, 0, 1)
+        # gridBtcCheck.addWidget(Label(u'交易对基准价'), 1, 0)
+        # gridBtcCheck.addWidget(self.lineSymbolBasicPrice, 1, 1)
+        # gridBtcCheck.addWidget(Label(u'交易对最低价'), 2, 0)
+        # gridBtcCheck.addWidget(self.lineSymbolMinPrice, 2, 1)
+        # gridBtcCheck.addWidget(Label(u'交易对最高价'), 3, 0)
+        # gridBtcCheck.addWidget(self.lineSymbolMaxPrice, 3, 1)
+        # gridBtcCheck.addWidget(Label(u'比特币基准价(USDT)'), 4, 0)
+        # gridBtcCheck.addWidget(self.lineBtcBasicPrice, 4, 1)
+        # gridBtcCheck.addWidget(Label(u'买卖档数'), 5, 0)
+        # gridBtcCheck.addWidget(self.lineOrderLevel, 5, 1)
+        # gridBtcCheck.addWidget(Label(u'档间价差倍数'), 6, 0)
+        # gridBtcCheck.addWidget(self.lineOrderPriceGap, 6, 1)
+        # gridBtcCheck.addWidget(Label(u'每档挂单数量上限'), 7, 0)
+        # gridBtcCheck.addWidget(self.lineOrderVolumeMax, 7, 1)
+        # gridBtcCheck.addWidget(Label(u'每档挂单数量下限'), 8, 0)
+        # gridBtcCheck.addWidget(self.lineOrderVolumeMin, 8, 1)
+        # gridBtcCheck.addWidget(Label(u'比特币检查时间间隔(秒)'), 9, 0)
+        # gridBtcCheck.addWidget(self.lineBtcCheckInterval, 9, 1)
+        # gridBtcCheck.addWidget(Label(u'价格波动倍数'), 10, 0)
+        # gridBtcCheck.addWidget(self.linePriceRatio, 10, 1)
+        #
+        # gridSplit = SplitGrid()
+        #
+        # # 刷单相关设置
+        # gridSelfTrade = QtWidgets.QGridLayout()
+        # self.lineSelfTradeVolume = QtWidgets.QLineEdit()
+        # self.lineSelfTradeInterval = QtWidgets.QLineEdit()
+        # self.lineSelfTradeDayVolume = QtWidgets.QLineEdit()
+        #
+        # gridSelfTrade.addWidget(Label(u'每次刷单平均数量'), 0, 0)
+        # gridSelfTrade.addWidget(self.lineSelfTradeVolume, 0, 1)
+        # gridSelfTrade.addWidget(Label(u'刷单间隔时间(秒)'), 1, 0)
+        # gridSelfTrade.addWidget(self.lineSelfTradeInterval, 1, 1)
+        # gridSelfTrade.addWidget(Label(u'预估每日刷单数量'), 2, 0)
+        # gridSelfTrade.addWidget(self.lineSelfTradeDayVolume, 2, 1)
+        # self.lineSelfTradeDayVolume.setStyleSheet("color: yellow")
+        #
+        # self.buttonSaveSetting = QtWidgets.QPushButton(u'保存配置')
+        # self.buttonSaveSetting.setStyleSheet("background-color: blue")
         #buttonActivate = QtWidgets.QPushButton(u'激活算法')
-        self.buttonSwitchEngineStatus = QtWidgets.QPushButton(u'BTC跟随报价刷单引擎停止')
+        self.buttonSwitchEngineStatus = QtWidgets.QPushButton(u'开始搬砖')
 
         hbox = QtWidgets.QHBoxLayout()
         hbox.addStretch()
-        hbox.addWidget(self.buttonSaveSetting)
+        # hbox.addWidget(self.buttonSaveSetting)
         hbox.addWidget(self.buttonSwitchEngineStatus)
 
         vbox = QtWidgets.QVBoxLayout()
         vbox.addLayout(gridBtcCheck)
-        vbox.addLayout(gridSplit)
-        vbox.addLayout(gridSelfTrade)
+        # vbox.addLayout(gridSplit)
+        # vbox.addLayout(gridSelfTrade)
         vbox.addLayout(hbox)
         self.setLayout(vbox)
 
@@ -127,7 +127,7 @@ class BrickTradeManager(QtWidgets.QWidget):
 
         # 关联更新
         self.comboVtSymbol.currentIndexChanged.connect(self.getsettingForVTSymbol)
-        self.buttonSaveSetting.clicked.connect(self.saveSettingForVTSymbol)
+        # self.buttonSaveSetting.clicked.connect(self.saveSettingForVTSymbol)
         self.buttonSwitchEngineStatus.clicked.connect(self.switchEngineStatus)
 
     def getsettingForVTSymbol(self):
@@ -178,14 +178,14 @@ class BrickTradeManager(QtWidgets.QWidget):
         self.followBtcEngine.saveSetting()
 
     def switchEngineStatus(self):
-        self.updateEngineStatus()
         self.followBtcEngine.switchEngineStatus()
+        self.updateEngineStatus()
 
     def updateEngineStatus(self):
         """更新引擎状态"""
         if self.followBtcEngine.active:
-            self.buttonSwitchEngineStatus.setText(u'BTC跟随报价刷单引擎运行中')
+            self.buttonSwitchEngineStatus.setText(u'运行中')
             self.buttonSwitchEngineStatus.setStyleSheet("background-color: green")
         else:
-            self.buttonSwitchEngineStatus.setText(u'BTC跟随报价刷单引擎已停止')
+            self.buttonSwitchEngineStatus.setText(u'开始搬砖')
             self.buttonSwitchEngineStatus.setStyleSheet("background-color: gray")
